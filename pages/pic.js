a1675ef8c8c5890e898c7af9c937588df5b9773b
issayev <issayev@sheridancollege.ca>
2021-05-10 12:10:01 -0400

    loader link changed

--- pages/pic.js
+++ pages/pic.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
 
 const myLoader = ({ src, width, quality }) => {
-  return `http://clean-nextjs-bucket.s3-website.us-east-2.amazonaws.com/${src}?w=${width}&q=${
-    quality || 75
-  }`;
+    return `http://clean-nextjs-bucket.s3-website.us-east-2.amazonaws.com/${src}?w=${width}&q=${
+      quality || 75
+    }`;
+  //return `http://localhost:3000/${src}?w=${width}&q=${quality || 75}`;
 };
 
 const MyImage = (props) => {
